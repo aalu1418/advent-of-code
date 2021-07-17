c1cd211ed38afdb010919e98e2736e88f4978f49
aalu1418 <aalu1418@gmail.com>
2021-07-16 23:19:54 -0400

    first attempt at 2015:15

--- y2015/d15.go
+++ y2015/d15.go
@@ -11,10 +11,42 @@ type ingredient struct {
 	c, d, f, t, cal int
 }
 
+func (i *ingredient) Max() {
+	if i.c < 0 {
+		i.c = 0
+	}
+
+	if i.d < 0 {
+		i.d = 0
+	}
+
+	if i.f < 0 {
+		i.f = 0
+	}
+
+	if i.t < 0 {
+		i.t = 0
+	}
+}
+
+func objFunc(coords map[string]int, consts *map[string]ingredient) int {
+	vals := ingredient{}
+	for k, v := range *consts {
+		vals.c += coords[k] * v.c
+		vals.d += coords[k] * v.d
+		vals.f += coords[k] * v.f
+		vals.t += coords[k] * v.t
+	}
+	vals.Max()
+	return vals.c * vals.d * vals.f * vals.t
+}
+
 // Fifteen implements the solution to day 15
 func Fifteen(input string) (out1 int, out2 int) {
 	// parse
 	ing := map[string]ingredient{}
+	coords := map[string]int{}
+	total := 100
 	for _, s := range strings.Split(input, "\n") {
 		re := regexp.MustCompile(`-?\d+`)
 		v := re.FindAllString(s, -1)
@@ -27,9 +59,30 @@ func Fifteen(input string) (out1 int, out2 int) {
 			vI = append(vI, temp)
 		}
 		ing[k] = ingredient{vI[0], vI[1], vI[2], vI[3], vI[4]}
+		coords[k] = 1
+		total--
 	}
 
-	fmt.Println(ing)
+	for i := 0; i < total; i++ {
+		max := 0
+		maxCoords := map[string]int{}
+		for k := range coords {
+			inputs := map[string]int{}
+			for key, val := range coords {
+				inputs[key] = val
+			}
+			inputs[k]++
+
+			v := objFunc(inputs, &ing)
+			if v > max {
+				max = v
+				maxCoords = inputs
+			}
+		}
+		coords = maxCoords
+	}
+	fmt.Println(coords)
+	out1 = objFunc(coords, &ing)
 
 	return
 }
